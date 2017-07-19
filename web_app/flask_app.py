71a164f838927db9b92bfb88e08fcf33a383b6e3
Zack <zackakil94@gmail.com>
2017-07-19 23:56:58 +0100

    Update flask_app.py

--- web_app/flask_app.py
+++ web_app/flask_app.py
@@ -28,11 +28,16 @@ def meetup_predictor():
       passenger_class = int(request.form['class'])
       is_female = int(request.form['is_female'])
 
-      # run model with user data
+      # run model with user data and predict class (1 for survive or 0 for die)
       prediction = clf.predict([[age, fare, passenger_class, is_female]])
-      
+      # run model and get probabilities for each class
+      probabilities = clf.predict_proba([[age, fare, passenger_class, is_female]])
+        
+      # create the probabiliy message
+      probability_message = '({0} chance of survival)'.format(probabilities[0][1])
+    
       # Return message based on the prediction
       if prediction[0] == 1:
-        return 'You might survive!'
+        return 'You might survive! ' + probability_message
       else:
-        'You\'ll probebly die!'
+        return 'You\'ll probebly die! ' + probability_message
